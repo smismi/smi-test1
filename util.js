03c8f203104cd2ca9a5329c0deb3711d833f307d
smismi <smirnov.ru@gmail.com>
2012-10-02 23:55:10 +0400

    addevents removeevents addclass removeclass

--- util.js
+++ util.js
@@ -1,7 +1,56 @@
-/**
- * Created with JetBrains WebStorm.
- * User: asmirnov
- * Date: 02.10.12
- * Time: 15:53
- * To change this template use File | Settings | File Templates.
- */
+function $(el) {
+    return document.getElementById(el);
+}
+function $px(x) {
+    return x + 'px';
+}
+
+var addEvent, removeEvent;
+
+if (document.addEventListener) {
+    addEvent = function(el, type, handler) {
+        el.addEventListener(type, handler, false)
+    }
+    removeEvent = function(el, type, handler) {
+        el.removeEventListener(type, handler, false)
+    }
+} else {
+    addEvent = function(el, type, handler) {
+        el.attachEvent("on" + type, handler)
+    }
+    removeEvent = function(el, type, handler) {
+        el.detachEvent("on" + type, handler)
+    }
+}
+
+var addClass, removeClass
+
+addClass = function (el, clss) {
+    classes = getClasses(el);
+    if(classes.indexOf(clss) == -1) {
+        el.className += (' ' + clss);
+    }
+
+};
+removeClass = function (el, clss) {
+    classes = getClasses(el);
+    var ine = classes.indexOf(clss);
+    if (ine == -1) {
+
+    } else {
+        var classq = '';
+        for (i=0; i< classes.length; i++) {
+            if (i != ine) {
+                classq += ((i==0) ? '' : ' ') + classes[i];
+            }
+        }
+        el.className = classq;
+    }
+
+    console.log(el.className);
+};
+getClasses = function (el) {
+   var classes = el.className.split(" ");
+
+    return classes;
+};
